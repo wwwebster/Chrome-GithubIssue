e80084e8981fa56d4f5f1482672c733f09138fa3
James Webster <james@aceviral.com>
2013-02-02 09:52:00 +0000

    Added ReadMe, Improved code
    Improved code formatting
    Added icons for chrome
    Added Read me.

--- README.md
+++ README.md
@@ -1,4 +1,49 @@
-Chrome-GithubIssue
+Chrome - Github Issues
 ==================
 
 A Google Chrome extension for notifications about GitHub issues.
+
+---
+
+
+Installation
+------------
+
+* Download the [source files](https://github.com/JamesAceViral/Chrome-GithubIssue/archive/master.zip "Source")
+* Unzip the folder
+* Open [chrome://extensions](chrome://extensions) and select "Load unpacked extension..."
+* Navigate to the unzipped folder and select it.
+
+
+Login
+------------
+
+You will need to login and give permissions via GitHub for the extension to work. Click on the Browser Icon that 
+should have appeared on the right of the address bar.
+
+
+Logging out
+------------
+
+You can revoke permissions at any time by visiting [https://github.com/settings/applications](https://github.com/settings/applications) and clicking Revoke for "Google Chrome / Github"
+
+
+
+Uninstalling
+------------
+
+* Open [chrome://extensions](chrome://extensions) and click the trash can next to "Github Issues Notifier"
+
+
+---
+
+
+Functions
+==================
+
+* Presents a Desktop Notification when you receive a new issue
+* Permanantly displays a number on the Icon to indicate the current number of issues
+* Clicking the Icon will take you to your latest issue or just to http://github.com if you have no issues
+ * If you are not yet logged in, clicking the Icon will instead attempt to log you in.
+ 
+ 
--- background.js
+++ background.js
@@ -1,88 +1,82 @@
-var checking;
+
 var numberOfIssuesLastTime;
 var lastNotificationURL;
 var ignoreLoginUntilClick = true;
-
-
-var timeOnLastNotification;
-
 var tabID;
+
+var githubRequestPage = "https://github.com/login/oauth/authorize?client_id=be805dc8d38328a7a5f1&redirect_uri=http://ebsters.co.uk/github/loginCallback.php&scope=repo,user,notifications"
+
+
+function parseResponse(data)
+{
+    //Close the login tab if open
+    if (typeof tabID !== 'undefined') chrome.tabs.remove(tabID);
+    
+    if (data.response == "avBugs")    //Check that this is the right response.
+    {
+        var numberOfIssues = data.bugCount;
+        chrome.browserAction.setBadgeText({text: ""+numberOfIssues});
+        
+        if (numberOfIssues > numberOfIssuesLastTime)
+        {
+            var bugs = data.bugs;
+            
+            //Just show the most recent bug as a notification even if there was more than one
+            var bug = bugs[0];
+            var notification = webkitNotifications.createNotification("icon.png", bug.title, bug.body);
+            notification.onclick = function(){chrome.tabs.create({'url': bug.url});};
+            notification.show();
+            
+            timeOnLastNotification = bug.time;
+            lastNotificationURL = bug.url;
+        }
+        else if (numberOfIssues > 0)
+        {
+            //Store the most recent URL so that when browser action is clicked, it goes to that bug
+            lastNotificationURL = data.bugs[0].url;
+        }
+        numberOfIssuesLastTime = numberOfIssues;
+        ignoreLoginUntilClick = false;
+    }
+}
+
+function handleFail()
+{
+    //If the connection failed, attempt to login on next click
+    ignoreLoginUntilClick = true;
+}
 
 
 function updateBadge()
 {
-    $.get("https://github.com/login/oauth/authorize?client_id=be805dc8d38328a7a5f1&redirect_uri=http://ebsters.co.uk/github/loginCallback.php&scope=repo,user,notifications",
-          function(data)
-          {
-          chrome.tabs.remove(tabID);
-              checking = false;
-              if (data.response == "avBugs")
-              {
-                var numberOfIssues = data.bugCount;
-                chrome.browserAction.setBadgeText({text: ""+numberOfIssues});
-              
-                if (numberOfIssues > numberOfIssuesLastTime)
-                {
-                    var bugs = data.bugs;
-              
-                    for (var i = 0; i < 1; i++)
-                    {
-                        var bug = bugs[i];
-                        var notification = webkitNotifications.createNotification("icon.png", bug.title, bug.body);
-                        notification.onclick = function(){chrome.tabs.create({'url': bug.url});};
-                        notification.show();
-          
-                        timeOnLastNotification = bug.time;
-                        lastNotificationURL = bug.url;
-                    }
-                }
-                else if (numberOfIssues > 0)
-                {
-                    lastNotificationURL = data.bugs[0].url;
-                }
-                numberOfIssuesLastTime = numberOfIssues;
-                ignoreLoginUntilClick = false;
-              }
-          }, "json").fail(function()
-                          {
-                          ignoreLoginUntilClick = true;
-                          });
+    var request = $.get(githubRequestPage, parseResponse, "json");
+    request.fail(handleFail);
     
     setTimeout(updateBadge, 10000);
 }
 
-function checkLogin()
+
+function handleBrowserActionClick()
 {
-    //Stop multiple checks at once.
-    if (checking)
+    if (ignoreLoginUntilClick)
     {
+        chrome.tabs.create({'url': githubRequestPage},
+                           function(tab)
+                           {
+                           tabID = tab.id;
+                           updateBadge();
+                           });
         return;
     }
-    checking = true;
-    updateBadge();
+    
+    if (lastNotificationURL != null)
+        chrome.tabs.create({'url': lastNotificationURL});
+    else
+        chrome.tabs.create({'url': 'http://www.github.com'});
 }
 
 
-chrome.browserAction.onClicked.addListener(function()
- {
-                                           if (ignoreLoginUntilClick)
-                                           {
-                                           chrome.tabs.create({'url': 'https://github.com/login/oauth/authorize?client_id=be805dc8d38328a7a5f1&redirect_uri=http://ebsters.co.uk/github/loginCallback.php&scope=repo,user,notifications'},
-                                                              function(tab){
-                                                              tabID = tab.id;
-                                                              updateBadge();
-                                                              });
-                                               return;
-                                           }
-                                           
-                                           if (lastNotificationURL != null)
-                                                chrome.tabs.create({'url': lastNotificationURL});
-                                           else
-                                                chrome.tabs.create({'url': 'http://www.github.com'});
- });
 
-
-
-$(document).ready(function() {
-                  checkLogin();
-                  });
+//Start the listeners
+chrome.browserAction.onClicked.addListener(handleBrowserActionClick);
+$(document).ready(updateBadge);
